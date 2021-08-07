fb3d36a7c5d3bb70b8786152277fecd2ea5c6be1
Nathan Mariano <nathan.mariano@nathan.mariano>
2021-08-07 17:04:34 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to estudo-autodidata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/estudo_autodidata/hello_world.clj
@@ -0,0 +1,8 @@
+(ns estudo-autodidata.hello-world)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(println (foo "Nathan"))
