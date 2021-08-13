ccc3e58475b3073ca3313a1149a8bfd5a27bef27
NathanSaldanha <nmariano85@gmail.com>
2021-08-13 20:55:31 -0300

    Finalizo a os exemplos de condicionais.

--- src/estudo_autodidata/condicionais.clj
+++ src/estudo_autodidata/condicionais.clj
@@ -1,14 +1,32 @@
 (ns estudo-autodidata.condicionais)
 
-;ifrecebe um predicado ( trueou false) como o primeiro argumento. O segundo argumento será avaliado se o predicado for avaliado como true.
+;O if ecebe um predicado (true ou false) como o primeiro argumento. O segundo argumento será avaliado se o predicado for avaliado como true.
 ;O terceiro argumento é equivalente a else em muitas linguagens de programação, que é avaliado quando o predicado é avaliado como false.
 (if true
   (println "This is always printed")
   (println "This is never printed"))
 
+(if false
+  (println "This is always printed")
+  (println "This is never printed"))
+
+(defn maior-que-zero [valor]
+  (if (> valor 0)
+    "verdadeiro"
+    "falso"))
+
+(println "Condição maior que zero DESCRITO:" (maior-que-zero -1))
+
+(defn maior-que-zero [valor]
+  (> valor 0))
+
+(println "Condição maior que zero SIMPLIFICADO:" (maior-que-zero 1))
+
+;O if-let vincula o resultado da condiçao ao pos-nums, que retorna uma coleção de numeros positivos.
+;O segundo argumento é para a outra condição. Ele será avaliado quando o primeiro argumento for avaliado como falso.
 (defn positive-number [numbers]
   (if-let [pos-nums (not-empty (filter pos? numbers))]
     pos-nums
-    "no positive numbers"))
+    "sem números positivo"))
 
-(positive-number-seq [-1 -2])
+(println (positive-number [-1 -2 1 2]))
